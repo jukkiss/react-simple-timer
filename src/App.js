de7eb6b8d10ec3ba6d395df06cf9f6fdcc2a8e48
Jukka <savela.j@gmail.com>
2024-05-04 20:58:47 +0300

    First fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimeOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{time}</div>
+      <div>
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimeOn(true)}>Start</button>
+        )}
+        {timerOn && <button onClick={() => setTimeOn(false)}>Stop</button>}
+        {!timerOn && time !== 0 && (
+          <button onClick={() => setTimeOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button onClick={() => setTime(0)}>Reset</button>
+        )}
+      </div>
+      
     </div>
   );
 }
